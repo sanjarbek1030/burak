2dba83013fe86e4a92442fdce22db734f074ae19
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-21 16:46:52 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,2 @@
-console.log("Hello, World");
-console.log("Let's learn TypeScript!");
-
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "Luffy";
-const count: number = 100;
+import dotenv from 'dotenv';
+dotenv.config();
