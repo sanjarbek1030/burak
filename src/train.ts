02042f556f317fa2f6c2b69a8b6db04eb6696db4
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-24 13:11:18 +0900

    feat: finished mitask-n

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,41 @@
+// MITASK-N
+// Function Declaration
+
+// 3rd attampt
+function palindromeCheck(str: string): boolean{
+  return str.split("").reverse().join("") === str
+}
+
+// 2rd attampt
+function palindromeCheck2(str: string): boolean{
+  const reverseStr: string = str.split("").reverse().join("");
+  return reverseStr === str;
+}
+
+// 1rd attampt
+function palindromeCheck3(str: string): boolean{
+  let arr: string[] = str.split("");
+  const arr_length: number = arr.length;
+  let new_arr = [];
+  for (let i = 0; i < arr_length; i++){
+      new_arr.push(arr.pop())
+  }
+  return new_arr.join("") === str;
+}
+
+// Testing
+// palindromeCheck 1
+console.log(palindromeCheck("dad")) // true
+console.log(palindromeCheck("son")) // false
+
+// palindromeCheck 2
+console.log(palindromeCheck2("radar")) // true
+console.log(palindromeCheck2("camera")) // false
+
+// palindromeCheck 3
+console.log(palindromeCheck3("2002")) // true
+console.log(palindromeCheck3("2001")) // false
+
 // New array
 const arr0920 = [1, 2, 3, 4];
 
@@ -26,4 +64,4 @@ interface NumberSquare {
   
 
 // Testing
-console.log(getSquareNumbers(arr0920));
+// console.log(getSquareNumbers(arr0920));
